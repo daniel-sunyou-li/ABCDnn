8cf63f5399aabcef87f301c57a0c7bb092f6867d
dsunyou <daniel_li2@brown.edu>
2022-02-07 08:03:53 -0600

    change name of application scripts

--- apply_abcdnn.py
+++ apply_abcdnn.py
@@ -1,150 +1,158 @@
-# apply a trained ABCDnn model to ttbar samples and create new step3's
-# last updated 10/25/2021 by Daniel Li
+# this script is run on the condor node for applying the trained ABCDnn model to ttbar samples
+# last updated 11/15/2021 by Daniel Li
 
-import glob, os, sys, subprocess
-import config
 import numpy as np
-from datetime import datetime
+import subprocess
+import os
+from tqdm.auto import tqdm
+import uproot
+import abcdnn
 from argparse import ArgumentParser
+from json import loads as load_json
+from array import array
 
-# read in arguments
+os.environ["KERAS_BACKEND"] = "tensorflow"
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
+import tensorflow as tf
+import config
+
 parser = ArgumentParser()
-parser.add_argument( "-y", "--year", required = True, default = "2017" )
-parser.add_argument( "-t", "--test", action = "store_true" )
-parser.add_argument( "-l", "--log", default= "application_log_" + datetime.now().strftime("%d.%b.%Y") )
-parser.add_argument( "-r", "--resubmit", action = "store_true" )
-parser.add_argument( "-c", "--checkpoints", nargs = "+", required = True, help = "Trained weight tag(s)" )
-parser.add_argument( "-loc", "--location", default = "LPC", help = "LPC,BRUX" )
+parser.add_argument( "-c", "--checkpoints", nargs = "+", required = True )
+parser.add_argument( "-y", "--year", required = True )
+parser.add_argument( "-l", "--location", default = "LPC" )
+parser.add_argument( "-s", "--storage", default = "LOCAL", help = "LOCAL,EOS" )
+parser.add_argument( "--test", action = "store_true" )
 args = parser.parse_args()
 
-# check if folder has necessary components
+import ROOT
 
-# paths 
-condorDir    = config.sourceDir[ "CONDOR" ]
-step3Samples = { "nominal": [] }
+# load in json file
 
-# determine which samples to run on
-if args.test: print( ">> Running in test mode..." )
-
-if args.location == "LPC":
-  sourceDir = config.sourceDir[ "LPC" ]
-elif args.location == "BRUX":
-  sourceDir = config.sourceDir[ "BRUX" ]
-
-
-print( ">> Running checkpoints: " )
-all_checkpoints = [ checkpoint.split( "." )[0] for checkpoint in os.listdir( "Results/" ) if ".data" in checkpoint ]
+models = {}
+transfers = {}
+transfer_errs = {}
+disc_tags = {}
+track_tags = []
+print( ">> Loading checkpoints to NAF models:" )
 for checkpoint in args.checkpoints:
-  if "." in checkpoint: 
-    print( "[WARN] Do not include file extension in -c (--checkpoints). Please resubmit {} as {}".format( checkpoint, checkpoint.split( "." )[0] ) )
-  elif checkpoint not in all_checkpoints:
-    print( "[ERR] {} is not a valid checkpoint. Please resubmit.".format( checkpoint ) )
-  else:
-    print( "  + {}".format( checkpoint ) )
-    
-if args.resubmit: 
-  print( "[OPT] Running in resubmit mode." )
-  print( ">> Resubmitting ABCDnn to samples:" )
-  doneSamples = {
-    tag: [ sample for sample in subprocess.check_output( "eos root://cmseos.fnal.gov ls /store/user/{}/{}/{}/".format( config.eosUserName, config.sampleDir[ args.year ], tag ), shell = True ).split( "\n" )[:-1] if "ABCDNN" in sample.upper() ] for tag in [ "nominal" ]
-  }
+  print( "  + {}".format( checkpoint ) )
+  with open( "Results/" + checkpoint + ".json", "r" ) as f:
+    params  = load_json( f.read() )
+    if params[ "DISC TAG" ][0] in track_tags:
+      print( "[WARN] Found redundant discriminator tag. Replacing {} --> {}".format( params[ "DISC TAG" ], checkpoint ) )
+      disc_tags[ checkpoint ] = checkpoint
+    else:
+      disc_tags[ checkpoint ] = params["DISC TAG"][0]
+    transfers[ checkpoint ] = float( params[ "TRANSFER" ] )
+    transfer_errs[ checkpoint ] = float( params[ "TRANSFER ERR" ] )
+    models[ checkpoint ] = abcdnn.NAF(
+      inputdim    = params["INPUTDIM"],
+      conddim     = params["CONDDIM"],
+      activation  = params["ACTIVATION"],
+      regularizer = params["REGULARIZER"],
+      nodes_cond  = params["NODES_COND"],
+      hidden_cond = params["HIDDEN_COND"],
+      nodes_trans = params["NODES_TRANS"],
+      depth       = params["DEPTH"],
+      permute = True
+    ) 
+  models[ checkpoint ].load_weights( "Results/" + checkpoint )
+
+
+
+# populate the step 3
+def fill_tree( sample ):
+  if args.storage == "EOS":
+    samples_done = subprocess.check_output( "eos root://cmseos.fnal.gov ls /store/user/{}/{}".format( config.eosUserName, config.sampleDir[ args.year ] ), shell = True ).split( "\n" )[:-1]
+    if sample in  samples_done:
+      print( ">> [WARN] {} already processed".format( sample ) )
+      return
+  print( ">> Formatting sample: {}".format( sample ) )
+  sample = os.path.join( config.sourceDir[ args.location ], config.sampleDir[ args.year ], sample )
+  upFile = uproot.open( sample )
+  upTree = upFile[ "ljmet" ]
+
+  variables = []
+  v_in = []
+  categorical = []
+  lowerlimit = []
+  upperlimit = []
+  predictions = {}
+
+  for variable in sorted( config.variables.keys() ):
+    if config.variables[ variable ][ "TRANSFORM" ]: v_in.append( variable )
+    variables.append( variable )
+    categorical.append( config.variables[ variable ][ "CATEGORICAL" ] )
+    upperlimit.append( config.variables[ variable ][ "LIMIT" ][1] )
+    lowerlimit.append( config.variables[ variable ][ "LIMIT" ][0] )
+
+  _onehotencoder = abcdnn.OneHotEncoder_int( categorical, lowerlimit = lowerlimit, upperlimit = upperlimit )
+
+  inputs_mc = upTree.pandas.df( variables )
+  x_upper = inputs_mc[ config.regions[ "X" ][ "VARIABLE" ] ].max() if config.regions[ "X" ][ "INCLUSIVE" ] else config.regions[ "X" ][ "MAX" ]
+  y_upper = inputs_mc[ config.regions[ "Y" ][ "VARIABLE" ] ].max() if config.regions[ "Y" ][ "INCLUSIVE" ] else config.regions[ "Y" ][ "MAX" ]
+  inputs_mc[ config.regions[ "X" ][ "VARIABLE" ] ].clip( config.regions[ "X" ][ "MIN" ], x_upper )
+  inputs_mc[ config.regions[ "Y" ][ "VARIABLE" ] ].clip( config.regions[ "Y" ][ "MIN" ], y_upper )
+
+  inputs_mc_enc = _onehotencoder.encode( inputs_mc.to_numpy( dtype = np.float32 ) )
+
+  print( ">> Applying normalization to MC inputs..." )
+  inputmeans = np.hstack( [ float( mean ) for mean in params[ "INPUTMEANS" ] ] )
+  inputsigma = np.hstack( [ float( sigma ) for sigma in params[ "INPUTSIGMAS" ] ] )
+  normedinputs_mc = ( inputs_mc_enc - inputmeans ) / inputsigma
+
+  print( "[START] Predicting using model checkpoints" )
+  for checkpoint in args.checkpoints:
+    predictions[ checkpoint ] = models[ checkpoint ].predict( normedinputs_mc )[:,0:2] * inputsigma[0:2] + inputmeans[0:2]
+
+  rFile_in = ROOT.TFile.Open( sample )
+  rTree_in = rFile_in.Get( "ljmet" )
+
+  rFile_out = ROOT.TFile( sample.replace( "hadd.root", "ABCDnn_hadd.root" ).split("/")[-1],  "RECREATE" )
+  rFile_out.cd()
+  rTree_out = rTree_in.CloneTree(0)
+
+  print( "[START] Adding new branches to {} ({} entries):".format( sample.split("/")[-1], rTree_in.GetEntries() ) )
+  arrays = {}
+  branches = {}
+  for checkpoint in args.checkpoints:
+    arrays[ checkpoint ] = { 
+      "transfer_{}".format( disc_tags[ checkpoint ] ): array( "f", [0.] ),
+      "transfer_err_{}".format( disc_tags[ checkpoint ] ): array( "f", [0.] )
+    }
+    branches[ checkpoint ] = {
+      "transfer_{}".format( disc_tags[ checkpoint ] ): rTree_out.Branch( "transfer_{}".format( disc_tags[ checkpoint ] ), arrays[ checkpoint ][ "transfer_{}".format( disc_tags[ checkpoint ] ) ], "transfer_{}/F".format( disc_tags[ checkpoint ] ) ),
+      "transfer_err_{}".format( disc_tags[ checkpoint ] ): rTree_out.Branch( "transfer_err_{}".format( disc_tags[ checkpoint ] ), arrays[ checkpoint ][ "transfer_err_{}".format( disc_tags[ checkpoint ] ) ], "transfer_err_{}/F".format( disc_tags[ checkpoint ] ) ) 
+    }
+    print( "  + transfer_{}".format( disc_tags[ checkpoint ] ) )
+    print( "  + transfer_err_{}".format( disc_tags[ checkpoint ] ) )
+    for variable in v_in:
+      arrays[ checkpoint ][ variable ] = array( "f", [0.] )
+      branches[ checkpoint ][ variable ] = rTree_out.Branch( "{}_{}".format( str( variable ), disc_tags[ checkpoint ] ) , arrays[ checkpoint ][ variable ], "{}_{}/F".format( str( variable ), disc_tags[ checkpoint ] ) );
+      print( "  + {}_{}".format( str( variable ), disc_tags[ checkpoint ] ) )    
+  print( ">> Looping through events" )
+  for i in tqdm( range( rTree_in.GetEntries() ) ): 
+    rTree_in.GetEntry(i)
+    for checkpoint in args.checkpoints:
+      arrays[ checkpoint ][ "transfer_{}".format( disc_tags[ checkpoint ] ) ][0] = transfers[ checkpoint ]
+      arrays[ checkpoint ][ "transfer_err_{}".format( disc_tags[ checkpoint ] ) ][0] = transfer_errs[ checkpoint ]
+      for j, variable in enumerate( v_in ):
+        arrays[ checkpoint ][ variable ][0] = predictions[ checkpoint ][i][j]
+    rTree_out.Fill()
+  
+  rTree_out.Write()
+  rFile_out.Write()
+  rFile_out.Close()
+  if args.storage == "EOS":
+    os.system( "xrdcp -vp {} {}".format( sample, os.path.join( config.sourceDir[ "CONDOR" ], config.sampleDir[ args.year ] ) ) )
+    os.system( "rm {}".format( sample.split( "/" )[-1].replace( "hadd", "ABCDnn_hadd" ) ) )
+  del rTree_in, rFile_in, rTree_out, rFile_out
+
+if args.test:
+  fill_tree( config.samples_apply[ args.year ][0] )
+else:
   for sample in config.samples_apply[ args.year ]:
-    if sample.replace( "hadd", "ABCDnn_hadd" ) not in doneSamples[ "nominal" ]: 
-      step3Samples[ "nominal" ].append( sample )
-else: 
-  print( "[OPT] Running in submit mode." )
-  print( ">> Applying ABCDnn to samples:" )
-  step3Samples[ "nominal" ] = [ config.samples_apply[ args.year ][0] ] if args.test else config.samples_apply[ args.year ]
+    fill_tree( sample )
+  
 
-for sample in step3Samples[ "nominal" ]:
-  print( "  + {}".format( sample ) )
-
-# general methods
-def check_voms():
-  print( ">> Checking VOMS" )
-  try:
-    output = subprocess.check_output( "voms-proxy-info", shell = True )
-    if output.rfind( "timeleft" ) > - 1:
-      if int( output[ output.rfind(": ")+2: ].replace( ":", "" ) ) > 0:
-        print( "[OK ] VOMS found" )
-        return True
-      return False
-  except: return False
-
-def voms_init():
-  if not check_voms():
-    print( ">> Initializing VOMS" )
-    output = subprocess.check_output( "voms-proxy-init --voms cms", shell = True )
-    if "failure" in output:
-      print( "[WARN] Incorrect password entered. Try again." )
-      voms_init()
-    print( "[OK] VOMS initialized" )
-
-def create_tar():
-  tarDir = os.getcwd()
-  if "ABCDnn.tgz" in os.listdir( os.getcwd() ): os.system( "rm ABCDnn.tgz" ) 
-  os.system( "tar -C {} -zcvf ABCDnn.tgz --exclude=\"{}\" --exclude=\"{}\" --exclude=\"{}\" --exclude=\"{}\" {}".format(
-      os.path.join( os.getcwd(), "../../../" ),
-      "ABCDnn/Data/*",
-      "ABCDnn/Results/*",
-      "ABCDnn/*log*/*",
-      "*.tgz",
-      "CMSSW_10_6_19/"
-    )
-  )
-  print( ">> Transferring ABCDnn.tgz to EOS" )
-  os.system( "xrdcp -f ABCDnn.tgz root://cmseos.fnal.gov//store/user/{}".format( config.eosUserName ) )
-
-def condor_job( fileName, condorDir, sampleDir, logDir, checkpoints, tag ):
-  request_memory = "5120" 
-  if "tttosemilepton" in fileName.lower() and "ttjj" in fileName.lower(): request_memory = "10240" 
-  if args.resubmit != None: request_memory = "16384"
-  dict = {
-    "SAMPLENAMEIN"  : fileName,
-    "SAMPLENAMEOUT" : fileName.replace( "hadd", "ABCDnn_hadd" ),
-    "CONDORDIR"     : condorDir,           
-    "SAMPLEDIR"     : sampleDir,   
-    "TAG"           : tag,
-    "CHECKPOINTS"   : checkpoints,
-    "CKP_JSON"      : ", ".join( [ "Results/" + checkpoint + ".json" for checkpoint in checkpoints ] ), 
-    "CKP_INDEX"     : ",  ".join( [ "Results/" + checkpoint + ".index" for checkpoint in checkpoints ] ), 
-    "CKP_DATA"      : ",  ".join( [ "Results/" + checkpoint + ".data-00000-of-00001" for checkpoint in checkpoints ] ),
-    "LOGDIR"        : logDir,              
-    "MEMORY"        : request_memory
-  }
-  jdfName = "{}/{}_{}.job".format( logDir, fileName.replace( "hadd", "ABCDnn_hadd" ), tag )
-  jdf = open( jdfName, "w" )
-  jdf.write(
-"""universe = vanilla
-Executable = apply_abcdnn.sh
-Should_Transfer_Files = Yes
-Transfer_Input_Files = %(CKP_JSON)s, %(CKP_INDEX)s, %(CKP_DATA)s
-WhenToTransferOutput = ON_EXIT
-request_memory = %(MEMORY)s
-Output = %(LOGDIR)s/%(SAMPLENAMEOUT)s_%(TAG)s.out
-Error = %(LOGDIR)s/%(SAMPLENAMEOUT)s_%(TAG)s.err
-Log = %(LOGDIR)s/%(SAMPLENAMEOUT)s_%(TAG)s.log
-Notification = Never
-Arguments = %(CONDORDIR)s %(SAMPLENAMEIN)s %(SAMPLENAMEOUT)s %(SAMPLEDIR)s %(CHECKPOINTS)s
-Queue 1"""%dict
-  )
-  jdf.close()
-  os.system( "condor_submit {}".format( jdfName ) )
-
-def submit_jobs( files, key, checkpoints, condorDir, logDir, sampleDir ):
-  os.system( "mkdir -vp " + logDir )
-  jobCount = 0
-  print( "[START] Submitting {} Condor jobs: ".format( key ) )
-  for file in files[key]:
-    print( "  + {}".format( file ) )
-    condor_job( file.split(".")[0], condorDir, sampleDir, logDir, checkpoints, key )
-    jobCount += 1
-  print( "[DONE] {} jobs submitted.".format( jobCount ) )
-  return jobCount
-
-def main( files ):
-  create_tar()
-  count = submit_jobs( files, "nominal", args.checkpoints, condorDir, args.log, os.path.join( config.sourceDir[ args.location ], config.sampleDir[ args.year ] ) )
-
-voms_init()
-main( step3Samples )
---
+++ condor_abcdnn.py
@@ -0,0 +1,150 @@
+# apply a trained ABCDnn model to ttbar samples and create new step3's
+# last updated 10/25/2021 by Daniel Li
+
+import glob, os, sys, subprocess
+import config
+import numpy as np
+from datetime import datetime
+from argparse import ArgumentParser
+
+# read in arguments
+parser = ArgumentParser()
+parser.add_argument( "-y", "--year", required = True, default = "2017" )
+parser.add_argument( "-t", "--test", action = "store_true" )
+parser.add_argument( "-l", "--log", default= "application_log_" + datetime.now().strftime("%d.%b.%Y") )
+parser.add_argument( "-r", "--resubmit", action = "store_true" )
+parser.add_argument( "-c", "--checkpoints", nargs = "+", required = True, help = "Trained weight tag(s)" )
+parser.add_argument( "-loc", "--location", default = "LPC", help = "LPC,BRUX" )
+args = parser.parse_args()
+
+# check if folder has necessary components
+
+# paths 
+condorDir    = config.sourceDir[ "CONDOR" ]
+step3Samples = { "nominal": [] }
+
+# determine which samples to run on
+if args.test: print( ">> Running in test mode..." )
+
+if args.location == "LPC":
+  sourceDir = config.sourceDir[ "LPC" ]
+elif args.location == "BRUX":
+  sourceDir = config.sourceDir[ "BRUX" ]
+
+
+print( ">> Running checkpoints: " )
+all_checkpoints = [ checkpoint.split( "." )[0] for checkpoint in os.listdir( "Results/" ) if ".data" in checkpoint ]
+for checkpoint in args.checkpoints:
+  if "." in checkpoint: 
+    print( "[WARN] Do not include file extension in -c (--checkpoints). Please resubmit {} as {}".format( checkpoint, checkpoint.split( "." )[0] ) )
+  elif checkpoint not in all_checkpoints:
+    print( "[ERR] {} is not a valid checkpoint. Please resubmit.".format( checkpoint ) )
+  else:
+    print( "  + {}".format( checkpoint ) )
+    
+if args.resubmit: 
+  print( "[OPT] Running in resubmit mode." )
+  print( ">> Resubmitting ABCDnn to samples:" )
+  doneSamples = {
+    tag: [ sample for sample in subprocess.check_output( "eos root://cmseos.fnal.gov ls /store/user/{}/{}/{}/".format( config.eosUserName, config.sampleDir[ args.year ], tag ), shell = True ).split( "\n" )[:-1] if "ABCDNN" in sample.upper() ] for tag in [ "nominal" ]
+  }
+  for sample in config.samples_apply[ args.year ]:
+    if sample.replace( "hadd", "ABCDnn_hadd" ) not in doneSamples[ "nominal" ]: 
+      step3Samples[ "nominal" ].append( sample )
+else: 
+  print( "[OPT] Running in submit mode." )
+  print( ">> Applying ABCDnn to samples:" )
+  step3Samples[ "nominal" ] = [ config.samples_apply[ args.year ][0] ] if args.test else config.samples_apply[ args.year ]
+
+for sample in step3Samples[ "nominal" ]:
+  print( "  + {}".format( sample ) )
+
+# general methods
+def check_voms():
+  print( ">> Checking VOMS" )
+  try:
+    output = subprocess.check_output( "voms-proxy-info", shell = True )
+    if output.rfind( "timeleft" ) > - 1:
+      if int( output[ output.rfind(": ")+2: ].replace( ":", "" ) ) > 0:
+        print( "[OK ] VOMS found" )
+        return True
+      return False
+  except: return False
+
+def voms_init():
+  if not check_voms():
+    print( ">> Initializing VOMS" )
+    output = subprocess.check_output( "voms-proxy-init --voms cms", shell = True )
+    if "failure" in output:
+      print( "[WARN] Incorrect password entered. Try again." )
+      voms_init()
+    print( "[OK] VOMS initialized" )
+
+def create_tar():
+  tarDir = os.getcwd()
+  if "ABCDnn.tgz" in os.listdir( os.getcwd() ): os.system( "rm ABCDnn.tgz" ) 
+  os.system( "tar -C {} -zcvf ABCDnn.tgz --exclude=\"{}\" --exclude=\"{}\" --exclude=\"{}\" --exclude=\"{}\" {}".format(
+      os.path.join( os.getcwd(), "../../../" ),
+      "ABCDnn/Data/*",
+      "ABCDnn/Results/*",
+      "ABCDnn/*log*/*",
+      "*.tgz",
+      "CMSSW_10_6_19/"
+    )
+  )
+  print( ">> Transferring ABCDnn.tgz to EOS" )
+  os.system( "xrdcp -f ABCDnn.tgz root://cmseos.fnal.gov//store/user/{}".format( config.eosUserName ) )
+
+def condor_job( fileName, condorDir, sampleDir, logDir, checkpoints, tag ):
+  request_memory = "5120" 
+  if "tttosemilepton" in fileName.lower() and "ttjj" in fileName.lower(): request_memory = "10240" 
+  if args.resubmit != None: request_memory = "16384"
+  dict = {
+    "SAMPLENAMEIN"  : fileName,
+    "SAMPLENAMEOUT" : fileName.replace( "hadd", "ABCDnn_hadd" ),
+    "CONDORDIR"     : condorDir,           
+    "SAMPLEDIR"     : sampleDir,   
+    "TAG"           : tag,
+    "CHECKPOINTS"   : checkpoints,
+    "CKP_JSON"      : ", ".join( [ "Results/" + checkpoint + ".json" for checkpoint in checkpoints ] ), 
+    "CKP_INDEX"     : ",  ".join( [ "Results/" + checkpoint + ".index" for checkpoint in checkpoints ] ), 
+    "CKP_DATA"      : ",  ".join( [ "Results/" + checkpoint + ".data-00000-of-00001" for checkpoint in checkpoints ] ),
+    "LOGDIR"        : logDir,              
+    "MEMORY"        : request_memory
+  }
+  jdfName = "{}/{}_{}.job".format( logDir, fileName.replace( "hadd", "ABCDnn_hadd" ), tag )
+  jdf = open( jdfName, "w" )
+  jdf.write(
+"""universe = vanilla
+Executable = apply_abcdnn.sh
+Should_Transfer_Files = Yes
+Transfer_Input_Files = %(CKP_JSON)s, %(CKP_INDEX)s, %(CKP_DATA)s
+WhenToTransferOutput = ON_EXIT
+request_memory = %(MEMORY)s
+Output = %(LOGDIR)s/%(SAMPLENAMEOUT)s_%(TAG)s.out
+Error = %(LOGDIR)s/%(SAMPLENAMEOUT)s_%(TAG)s.err
+Log = %(LOGDIR)s/%(SAMPLENAMEOUT)s_%(TAG)s.log
+Notification = Never
+Arguments = %(CONDORDIR)s %(SAMPLENAMEIN)s %(SAMPLENAMEOUT)s %(SAMPLEDIR)s %(CHECKPOINTS)s
+Queue 1"""%dict
+  )
+  jdf.close()
+  os.system( "condor_submit {}".format( jdfName ) )
+
+def submit_jobs( files, key, checkpoints, condorDir, logDir, sampleDir ):
+  os.system( "mkdir -vp " + logDir )
+  jobCount = 0
+  print( "[START] Submitting {} Condor jobs: ".format( key ) )
+  for file in files[key]:
+    print( "  + {}".format( file ) )
+    condor_job( file.split(".")[0], condorDir, sampleDir, logDir, checkpoints, key )
+    jobCount += 1
+  print( "[DONE] {} jobs submitted.".format( jobCount ) )
+  return jobCount
+
+def main( files ):
+  create_tar()
+  count = submit_jobs( files, "nominal", args.checkpoints, condorDir, args.log, os.path.join( config.sourceDir[ args.location ], config.sampleDir[ args.year ] ) )
+
+voms_init()
+main( step3Samples )
